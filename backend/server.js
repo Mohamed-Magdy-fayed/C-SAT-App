2bbf5f14afd76b6d631688fd9a4959c6ed27b9dd
muhammad magdy <89049735+Mohamed-Magdy-fayed@users.noreply.github.com>
2022-11-09 14:40:08 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
